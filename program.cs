f9644d2520c38e9efc1979b1bdfed445773616ac
CANBERK ÖZBEK <canberk4666@gmail.com>
2026-02-22 23:35:10 +0300

    Update program.cs

--- program.cs
+++ program.cs
@@ -1,1 +1,25 @@
+using System;
 
+class BankaHesabi
+{
+    public string HesapSahibi;
+    public double Bakiye;
+
+    public void ParaYatir(double miktar)
+    {
+        Bakiye += miktar;
+        Console.WriteLine("Yeni Bakiye: " + Bakiye + " TL");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        BankaHesabi hesap = new BankaHesabi();
+        hesap.HesapSahibi = "hasan";
+        hesap.Bakiye = 1000;
+
+        hesap.ParaYatir(200);
+    }
+}
